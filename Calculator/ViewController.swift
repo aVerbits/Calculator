a27f6685ddf0266acbc54a6f0f1358f2f6f6ad1b
aVerbits <56268791+aVerbits@users.noreply.github.com>
2019-10-09 19:42:16 +0300

    logic buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberFromScreen: Double = 0;
+    var firstNum: Double = 0;
+    var mathSign: Bool = false;
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        
+        if mathSign == true {
+            result.text = String (sender.tag)
+            mathSign = false
+
+        }
+        else{
+            result.text = result.text! + String (sender.tag)
+
+        }
+        
+        numberFromScreen = Double(result.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        
+        if result.text != ""{
+            if sender.tag == 11 {  // Деление
+            
+            }
+            else if sender.tag == 12 {  // Умножение
+                
+            }
+            else if sender.tag == 13 {  // Вычитание
+                
+            }
+            else if sender.tag == 14 {  // Добавление
+                result.text = "+"
+            }
+            
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
